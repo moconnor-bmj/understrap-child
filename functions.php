504071bf179efeccf31d9a9495282c2824632118
Michael OConnor <moconnor@bmj.com>
2016-12-02 11:44:38 +0000

    Responsive, Rebranded, ReThemed

--- functions.php
+++ functions.php
@@ -13,5 +13,99 @@ add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array());
+    wp_enqueue_style( 'child-understrap-font-styles', get_stylesheet_directory_uri() . '/css/fonts.css', array());
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Yanone+Kaffeesatz%3A200%2C300%2C400%2C700&#038;ver=4.5.4' );
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), '0.1.0', true );
 }
+
+
+/**
+ * Archive Post Class
+ * @since 1.0.0
+ *
+ * Breaks the posts into three columns
+ * @link http://www.billerickson.net/code/grid-loop-using-post-class
+ *
+ * @param array $classes
+ * @return array
+ * 
+ * @Added by: moconnor-bmj
+ */
+
+function be_archive_post_class( $classes ) {
+	global $wp_query;
+	if( ! $wp_query->is_main_query() )
+		return $classes;
+		
+    $classes[] = 'col-md-6';
+        if( 0 == $wp_query->current_post )
+            $classes[] = 'myFeaturedPosts';
+        return $classes;
+}
+add_filter( 'post_class', 'be_archive_post_class' ); 
+
+
+/**************************************************************************
+ * Add author box to end of posts
+ * 
+ *
+ * @Added by: moconnor-bmj
+ */
+        function wpb_author_info_box( $content ) {
+
+        global $post;
+
+        // Detect if it is a single post with a post author
+        if ( is_single() && isset( $post->post_author ) ) {
+
+        // Get author's display name 
+        $display_name = get_the_author_meta( 'display_name', $post->post_author );
+
+        // If display name is not available then use nickname as display name
+        if ( empty( $display_name ) )
+        $display_name = get_the_author_meta( 'nickname', $post->post_author );
+
+        // Get author's biographical information or description
+        $user_description = get_the_author_meta( 'user_description', $post->post_author );
+
+        // Get author's website URL 
+        $user_website = get_the_author_meta('url', $post->post_author);
+
+        // Get link to the author archive page
+        $user_posts = get_author_posts_url( get_the_author_meta( 'ID' , $post->post_author));
+        
+        if ( ! empty( $display_name ) )
+
+        $author_details = '<p class="author_name">About ' . $display_name . '</p>';
+
+        if ( ! empty( $user_description ) )
+        // Author avatar and bio
+
+        $author_details .= '<p class="author_details">' . get_avatar( get_the_author_meta('user_email') , 90 ) . nl2br( $user_description ). '</p>';
+
+        $author_details .= '<p class="author_links"><a href="'. $user_posts .'">View all posts by ' . $display_name . '</a>';  
+
+        // Check if author has a website in their profile
+        if ( ! empty( $user_website ) ) {
+
+        // Display author website link
+        $author_details .= ' | <a href="' . $user_website .'" target="_blank" rel="nofollow">Website</a></p>';
+
+        } else { 
+        // if there is no author website then just close the paragraph
+        $author_details .= '</p>';
+        }
+
+        // Pass all this info to post content  
+        $content = $content . '<footer class="author_bio_section" >' . $author_details . '</footer>';
+        }
+        return $content;
+        }
+
+        // Add our function to the post content filter 
+        add_action( 'the_content', 'wpb_author_info_box' );
+
+        // Allow HTML in author bio section 
+        remove_filter('pre_user_description', 'wp_filter_kses');
+
+
---
+++ header.php
@@ -0,0 +1,78 @@
+<?php //touch('wp-content/themes/understrap-child/category-patient_perspectives.php'); ?>
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+?>
+
+<?php $container = get_theme_mod('understrap_container_type'); ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+  <div class="hfeed site" id="page">
+
+    <!-- ******************* The Navbar Area ******************* -->
+    <div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
+
+      <a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content', 'understrap' ); ?></a>
+	
+	<!-- ******************* LEADERBOARD - remove to include file !!! ******************* -->
+	<div class="ad-leaderboard hidden-sm-down" style="display: block; background-color: #ddd; text-align: center; padding: 1.52rem 0;">
+         <img src="http://placehold.it/728x90/52c9f5/ffffff?text=Leaderboard"></div>
+    </div>
+    <!-- ******************* END LEADERBOARD - remove to include file !!! ******************* -->
+
+
+	  <nav class="navbar navbar-dark bg-primary site-navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+
+        <div class="container" id="content">
+
+          <div class="navbar-header">
+
+            <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+            <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target=".exCollapsingNavbar" aria-controls="exCollapsingNavbar" aria-expanded="false" aria-label="Toggle navigation"></button>
+
+            <!-- Your site title as branding in the menu -->
+            <?php if (!has_custom_logo()) { ?>
+              <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+                <?php bloginfo( 'name' ); ?>
+              </a>
+            <?php } else { the_custom_logo(); } ?><!-- end custom logo -->
+
+          </div>
+
+          <!-- The WordPress Menu goes here -->
+          <?php wp_nav_menu(
+            array(
+              'theme_location' => 'primary',
+              'container_class' => 'collapse navbar-toggleable-xs exCollapsingNavbar',
+              'menu_class' => 'nav navbar-nav',
+              'fallback_cb' => '',
+              'menu_id' => 'main-menu',
+              'walker' => new wp_bootstrap_navwalker()
+            )
+          ); ?>
+
+        </div> <!-- .container -->
+
+      </nav><!-- .site-navigation -->
+
+    </div><!-- .wrapper-navbar end -->
